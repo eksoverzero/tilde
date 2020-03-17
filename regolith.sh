430afa0f7783b7ee897c56bcbe52943336dd92be
Steven Eksteen <eksoverzero.zero@gmail.com>
2020-03-17 14:07:31 +0200

    Update keybinds
    Selects the previous workspace

--- regolith.sh
+++ regolith.sh
@@ -2,5 +2,5 @@
 
 rm -fR ~/.config/regolith && \
 ln -s $PWD/.config/regolith ~/.config/regolith
-ln -s $PWD/.Xresources-regolith ~/.Xresources-regolith
+ln -s $PWD/.Xresources ~/.Xresources-regolith
 ln -s $PWD/.Xresources.d ~/.Xresources.d
